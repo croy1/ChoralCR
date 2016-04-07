25678bd9117b3d6410b985e8245e3917e5bd2205
croy1 <Guest@stk-cs01-ws10.westlothian.sch.uk>
2016-04-07 12:01:59 +0100

    chloralsprint5.4

---
+++ __SHELL5.java
@@ -0,0 +1,10 @@
+
+public class __SHELL5 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("/Users/Guest/Documents/ChoralCR");
+final CONCERT cONCERT1 = (CONCERT)__bluej_runtime_scope.get("cONCERT1");
+
+
+cONCERT1.processCustomers();
+
+}}
